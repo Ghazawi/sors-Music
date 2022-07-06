4b4c9cbbf8d8463a3ac3fe7a481c4b02592a3cb5
Ghazawi vip <92712096+Ghazawi@users.noreply.github.com>
2022-07-06 23:53:41 +0300

    Update start.py

--- R0R77/plugins/start.py
+++ R0R77/plugins/start.py
@@ -24,7 +24,7 @@ async def start(event):
              caption=PM_START_TEXT.format(event.sender.first_name), 
              buttons=[
         [Button.url("➕ اضغط هنا لأضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://github.com/R0R77/Telethon-Music")],
+        [Button.url("السورس", "https://github.com/Ghazawi/sors-Music")],
         [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
         [Button.inline("الاوامر", data="help")]])
        return
@@ -40,7 +40,7 @@ async def _(event):
     if event.is_private:
        await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.url("➕ اضغط هنا لاضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://github.com/R0R77/Telethon-Music")],
+        [Button.url("السورس", "https://github.com/Ghazawi/sors-Music")],
         [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
         [Button.inline("الاوامر", data="help")]])
        return
